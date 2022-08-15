9704f123564958445b72ce52f9a8c187563bc81d
李沁芳 <liqinfang@192-168-1-100.tpgi.com.au>
2022-08-15 21:17:33 +1000

    update

--- app.js
+++ app.js
@@ -14,3 +14,7 @@ app.get("/", function(req, res) {
 app.listen(process.env.PORT || 3000, function() {
   console.log("Port gets started.");
 });
+
+// heroku login
+// heroku git:remote -a <YOUR PROJECT NAME>
+// git push -f heroku master
